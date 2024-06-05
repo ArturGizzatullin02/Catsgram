d6bcffabe44bb4077575d80c560b00caae7ff1b0
Artur Gizzatullin <salatikufa@yandex.ru>
2024-06-05 18:57:37 +0500

    add service layer

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,70 +1,37 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
 
-import java.time.Instant;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    Map<Long, User> users = new HashMap<>();
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
     public Collection<User> getUsers() {
-        return users.values();
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
     }
 
     @PostMapping
     public User addUser(@RequestBody User user) {
-        if (user.getEmail() == null) {
-            throw new ConditionsNotMetException("Имейл должен быть указан");
-        } else if (emailValidation(user.getEmail())) {
-            throw new ConditionsNotMetException("Этот имейл уже используется");
-        }
-        user.setId(getNextId());
-        user.setRegistrationDate(Instant.now());
-        users.put(user.getId(), user);
-        return user;
+        return userService.addUser(user);
     }
 
     @PutMapping
     public User updateUser(@RequestBody User user) {
-        if (user.getId() == null) {
-            throw new ConditionsNotMetException("ID должен быть указан");
-        } else if (emailValidation(user.getEmail())) {
-            throw new ConditionsNotMetException("Этот имейл уже используется");
-        } else if (user.getId() == null) {
-            return user;
-        } else if (user.getEmail() == null) {
-            return user;
-        } else if (user.getPassword() == null) {
-            return user;
-        } else {
-            users.put(user.getId(), user);
-            return user;
-        }
-    }
-
-    private boolean emailValidation(String email) {
-//        return users.values().stream()
-//                .noneMatch(user -> user.getEmail().equals(email));
-        for (User user : users.values()) {
-            if (user.getEmail().equals(email)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private long getNextId() {
-        long currentMaxId = users.keySet().stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+        return userService.updateUser(user);
     }
 }
